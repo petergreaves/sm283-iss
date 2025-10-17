2d2600a82c37564f2a1c9ee9dafe190251db15e0
petergreaves <peter.greaves64@gmail.com>
2025-10-17 13:51:07 +0100

    added missing type suffixes to moon and asteroid labels

--- ch1/plotting.py
+++ ch1/plotting.py
@@ -44,11 +44,12 @@ moons_radii = np.array([11.1,6.2,1821,
                         585,789,761,209,
                         1353,170,606],float)
 
-moons_labels = ["Ph","De","Io","Eu","Ga","Ca","Mi","En","Te","Di","Rh",	"Ti","Ia","Mr","Ar","Um","Tt","Ob","Pr","Tr", "Nr","Ch"]
+moons_labels = ["Ph-M","De-M","Io-M","Eu-M","Ga-M","Ca-M","Mi-M","En-M","Te-M","Di-M","Rh-M",
+                "Ti-M","Ia-M","Mr-M","Ar-M","Um-M","Tt-M","Ob-M","Pr-M","Tr-M", "Nr-M","Ch-M"]
 
 asteroids_densities = np.array([2.50,2.60,1.30,2.67,1.90,3.40,3.46,2.16], float)
 asteroids_radii = np.array([6.1,15.8,26.4,9.69,0.32,50,263,473], float)
-asteroids_labels = ["Ga",	"Id",	"Ma",	"Er",	"It",	"Lu",	"Vs",	"Ce"]
+asteroids_labels = ["Ga-A",	"Id-A",	"Ma-A",	"Er-A",	"It-A",	"Lu-A",	"Vs-A",	"Ce-A"]
 #print(len(asteroids_radii_native)==len(asteroids_log_radii) and len(moons_log_radii)==len(moons_radii_native) and len(planets_log_radii)==len(planets_radii_native))
 #print(str(len(moons_radii)), ",", str(len(moons_densities)), ",", str(len(moons_labels)))
 
